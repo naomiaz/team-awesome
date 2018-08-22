584db85ee82eceadf2ff9c2b25c2fe7b0b841e24
naomiaz <naomizuiverloon@gmail.com>
2018-08-22 22:02:10 +0200

    Converted functional component to class component

--- components/time-entry-detail/TimeEntryDetail.jsx
+++ components/time-entry-detail/TimeEntryDetail.jsx
@@ -2,62 +2,68 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { calculateDuration } from '../../services/date-time/date-time';
 
-
 import './time-entry-detail.scss';
 
 const timeOptions = { hour: 'numeric', minute: 'numeric' };
 
-const TimeEntryDetail = ({
-  client, handleEntryDelete, id, timeFrom, timeTo
-}) => {
-  const handleClick = () => {
+class TimeEntryDetail extends React.Component {
+  static propTypes = {
+    id: PropTypes.number.isRequired,
+    client: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    timeFrom: PropTypes.string.isRequired,
+    timeTo: PropTypes.string.isRequired,
+    handleEntryDelete: PropTypes.func.isRequired
+  }
+
+  handleClick = () => {
+    const {
+      client, date, handleEntryDelete, id, timeFrom, timeTo
+    } = this.props;
     console.log(id);
     const deleteEntryPrompt = prompt(`Are you sure you want to remove this entry?
-    ${client} (${new Date(timeFrom).toLocaleTimeString('nl-NL', timeOptions)} - ${new Date(timeTo).toLocaleTimeString('nl-NL', timeOptions)})`, 'OK, delete this entry');
+    ${client} on ${new Date(date).toLocaleDateString('nl-NL')} (${new Date(timeFrom).toLocaleTimeString('nl-NL', timeOptions)} - ${new Date(timeTo).toLocaleTimeString('nl-NL', timeOptions)})`, 'OK, delete this entry');
+
     if (deleteEntryPrompt !== null) {
       handleEntryDelete(id);
     }
   };
 
-  return (
-    <div className="time-entry__project">
-      <div className="time-entry__client">
-        <p>
-          {client}
-        </p>
+  render() {
+    const { client, timeFrom, timeTo } = this.props;
+    return (
+      <div className="time-entry__project">
+        <div className="time-entry__client">
+          <p>
+            {client}
+          </p>
 
-        <button
-          className="btn time-entry__button-delete"
-          onClick={handleClick}
-          type="button"
-        >
-          <svg className="time-entry__icon--delete" />
-          Delete
-        </button>
+          <button
+            className="btn time-entry__button-delete"
+            onClick={this.handleClick}
+            type="button"
+          >
+            <svg className="time-entry__icon--delete" />
+            Delete
+          </button>
 
+        </div>
+        <div className="time-entry__time">
+          <p>
+            {`
+              ${new Date(timeFrom).toLocaleTimeString('nl-NL', timeOptions)}
+              -
+              ${new Date(timeTo).toLocaleTimeString('nl-NL', timeOptions)}
+            `}
+          </p>
+          <p className="time-entry__duration">
+            {calculateDuration(timeFrom, timeTo)}
+          </p>
+        </div>
       </div>
-      <div className="time-entry__time">
-        <p>
-          {`
-            ${new Date(timeFrom).toLocaleTimeString('nl-NL', timeOptions)}
-            -
-            ${new Date(timeTo).toLocaleTimeString('nl-NL', timeOptions)}
-          `}
-        </p>
-        <p className="time-entry__duration">
-          {calculateDuration(timeFrom, timeTo)}
-        </p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-TimeEntryDetail.propTypes = {
-  id: PropTypes.number.isRequired,
-  client: PropTypes.string.isRequired,
-  timeFrom: PropTypes.string.isRequired,
-  timeTo: PropTypes.string.isRequired,
-  handleEntryDelete: PropTypes.func.isRequired
-};
 
 export default TimeEntryDetail;
