0554576b1660ae61f2f95ebe89bd3fbc120c55d4
naomiaz <naomizuiverloon@gmail.com>
2018-08-22 21:51:38 +0200

    Fix travis error

--- components/time-entry-form/TimeEntryForm.jsx
+++ components/time-entry-form/TimeEntryForm.jsx
@@ -50,7 +50,7 @@ class TimeEntryForm extends React.Component {
     event.preventDefault();
     // Deconstruct timeEntry from the state and handleEntrySubmit() from the props
     const { timeEntry } = this.state;
-    const { date, timeFrom, timeTo, id } = timeEntry;
+    const { date, timeFrom, timeTo } = timeEntry;
     const { handleEntrySubmit } = this.props;
     // Convert the dates/times to ISOStrings before sending the data back to the parent
     const dateFormatted = convertDateToIso(date);
@@ -60,7 +60,6 @@ class TimeEntryForm extends React.Component {
     const newTimeEntryFormatted = {
       ...timeEntry,
       date: dateFormatted,
-      id,
       timeFrom: timeFromFormatted,
       timeTo: timeToFormatted
     };
