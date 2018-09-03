a686c4348302a5b342a5f885b680d1babea929ce
naomiaz <naomizuiverloon@gmail.com>
2018-09-03 18:01:36 +0200

    Add toggle functionality to team member item

--- components/team-member-detail/TeamMemberDetail.jsx
+++ components/team-member-detail/TeamMemberDetail.jsx
@@ -6,51 +6,63 @@ class TimeEntryDetail extends React.Component {
   static propTypes = {
   }
 
+  state = {
+    isDetailWrapperVisible: false
+  }
+
+  toggleDetailWrapper = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      isDetailWrapperVisible: !prevState.isDetailWrapperVisible
+    }));
+  };
+
   render() {
+    const { isDetailWrapperVisible } = this.state;
     return (
-      <div className="team-member__row">
+      <div className="team-member-item">
         {/* MAIN WRAPPER */}
-        <div className="team-member-main-wrapper">
+        <div className="team-member-item__main-wrapper">
           {/* PERSONALIA */}
-          <div className="team-member-primary-wrapper">
+          <div className="team-member-item__primary-wrapper">
             <img
               alt=""
-              className="team-member__avatar"
+              className="team-member-item__avatar"
               src="../../static/images/avatar-naomi.jpg"
             />
-            <div className="team-member__personalia">
-              <p className="team-member__text--primary">
+            <div className="team-member-item__personalia">
+              <p className="team-member-item__text--primary">
                 Naomi Zuiverloon
               </p>
-              <p className="team-member__text--secondary">
+              <p className="team-member-item__text--secondary">
                 Front-end Developer
               </p>
             </div>
           </div>
 
           {/* WORK INFO */}
-          <div className="team-member-secondary-wrapper">
-            <div className="team-member__employer-info">
-              <p className="team-member__text--primary">
+          <div className="team-member-item__secondary-wrapper">
+            <div className="team-member-item__employer-info">
+              <p className="team-member-item__text--primary">
                 HUM_001
               </p>
-              <p className="team-member__text--secondary">
+              <p className="team-member-item__text--secondary">
                 Employee number
               </p>
             </div>
-            <div className="team-member__employer-info">
-              <p className="team-member__text--primary">
+            <div className="team-member-item__employer-info">
+              <p className="team-member-item__text--primary">
                 Hike One
               </p>
-              <p className="team-member__text--secondary">
+              <p className="team-member-item__text--secondary">
                 Current employer
               </p>
             </div>
-            <div className="team-member__employer-info">
-              <p className="team-member__text--primary">
+            <div className="team-member-item__employer-info">
+              <p className="team-member-item__text--primary">
                 February 2018
               </p>
-              <p className="team-member__text--secondary">
+              <p className="team-member-item__text--secondary">
                 Starting date
               </p>
             </div>
@@ -60,33 +72,31 @@ class TimeEntryDetail extends React.Component {
           <button
             className="btn caret-btn"
             name="button"
+            onClick={this.toggleDetailWrapper}
             type="button"
           >
-            <img
-              alt=""
-              src="../../static/icons/arrow-down-2.svg"
-            />
+            <svg className={`team-member-item__icon-caret${isDetailWrapperVisible ? '--up' : ''}`} />
           </button>
         </div>
 
         {/* EXPANDABLE WRAPPER */}
-        <div className="team-member-detail-wrapper--visible">
-          <div className="team-member__detail-title">
+        <div className={`team-member-item__detail-wrapper${isDetailWrapperVisible ? '--visible' : '--hidden'}`}>
+          <div className="team-member-item__detail-title">
             Detailed information about Naomi
           </div>
-          <div className="team-member__employer-info">
-            <p className="team-member__text--primary">
+          <div className="team-member-item__employer-info">
+            <p className="team-member-item__text--primary">
               Hike One
             </p>
-            <p className="team-member__text--secondary">
+            <p className="team-member-item__text--secondary">
               Current employer
             </p>
           </div>
-          <div className="team-member__employer-info">
-            <p className="team-member__text--primary">
+          <div className="team-member-item__employer-info">
+            <p className="team-member-item__text--primary">
               February 2018
             </p>
-            <p className="team-member__text--secondary">
+            <p className="team-member-item__text--secondary">
               Starting date
             </p>
           </div>
