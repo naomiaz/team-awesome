8851f33840b2fd9b0d53e583d4efb5b72afe432a
naomiaz <naomizuiverloon@gmail.com>
2018-09-04 11:08:33 +0200

    Refactor css of arrow animation

--- components/team-member-detail/TeamMemberDetail.jsx
+++ components/team-member-detail/TeamMemberDetail.jsx
@@ -75,7 +75,7 @@ class TimeEntryDetail extends React.Component {
             onClick={this.toggleDetailWrapper}
             type="button"
           >
-            <svg className={`team-member-item__icon-caret${isDetailWrapperVisible ? '--up' : '--down'}`} />
+            <svg className={`team-member-item__icon-caret team-member-item__icon-caret${isDetailWrapperVisible ? '--up' : '--down'}`} />
           </button>
         </div>
 
