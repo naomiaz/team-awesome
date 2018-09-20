27cd05ce393ecd04478e413a37e7b617cc9553bd
naomiaz <naomizuiverloon@gmail.com>
2018-09-20 16:21:41 +0200

    Update SelectBox file

--- shared/components/select-box/SelectBox.jsx
+++ shared/components/select-box/SelectBox.jsx
@@ -4,22 +4,13 @@ import PropTypes from 'prop-types';
 import './select-box.scss';
 
 const SelectBox = ({
-<<<<<<< HEAD
-  className, defaultValue, name, onChangeFunction, options
-=======
   className, selectedValue, name, onChange, options
->>>>>>> master
 }) => (
   <select
     className={`select-box ${className}`}
     name={name}
-<<<<<<< HEAD
-    onChange={(event) => onChangeFunction(event.target.value)}
-    value={defaultValue}
-=======
     onChange={(event) => onChange(event.target.value)}
     value={selectedValue}
->>>>>>> master
   >
     {options.map((currentOption) => (
       <option
@@ -39,15 +30,9 @@ SelectBox.defaultProps = {
 
 SelectBox.propTypes = {
   className: PropTypes.string,
-<<<<<<< HEAD
-  onChangeFunction: PropTypes.func.isRequired,
-  name: PropTypes.string,
-  defaultValue: PropTypes.string.isRequired,
-=======
   onChange: PropTypes.func.isRequired,
   name: PropTypes.string,
   selectedValue: PropTypes.string.isRequired,
->>>>>>> master
   options: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
