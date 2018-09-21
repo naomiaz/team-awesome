10899ba2772561e70c77645d93fae211431452ed
naomiaz <naomizuiverloon@gmail.com>
2018-09-21 17:57:15 +0200

    Fix Storybook configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,13 @@
-const path = require('path');
-module.exports = (baseConfig, configType) => {
-  baseConfig.resolve.modules.push(path.resolve(__dirname, '../src'));
-  baseConfig.module.rules.push(
-    {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'style-loader',
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            localIdentName: '[local]-[hash:base64:8]'
-          },
-        },
-        {
-          loader: 'sass-loader'
-        }
-      ],
-    }
-  );
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "../")
+      }
+    ]
+  }
 };
